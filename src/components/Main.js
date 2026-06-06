3dedbf86a7777244548d2714c1850dfa1eb40328
NamDo <166409645+Namdo2465@users.noreply.github.com>
2026-06-07 01:44:33 +0700

    Add projects section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import About from './About';
-import Contact from './Contact';
-import HeroSection from './HeroSection';
-import Skills from './Skills';
-import Education from './Education';
-import Experience from './Experience';
-import Achievements from './Achievements'; 
-import Chatbot from './Chatbot';
-// import Works from './Works';
+import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import HeroSection from "./HeroSection";
+import Skills from "./Skills";
+import Education from "./Education";
+import Experience from "./Experience";
+import Achievements from "./Achievements";
+import Chatbot from "./Chatbot";
+import Projects from "./Works";
 
-const Main = ({nav, handleNav, closeNav}) => {
-  return(
-    <div onClick={closeNav } className='main'>
-        <HeroSection nav={nav} handleNav={handleNav} />
-        <About />
-        <Education />
-        <Experience />
-        <Achievements />
-        <Skills />
-        {/* <Works /> */}
-        <Contact />
-        <Chatbot />
+const Main = ({ nav, handleNav, closeNav }) => {
+  return (
+    <div onClick={closeNav} className="main">
+      <HeroSection nav={nav} handleNav={handleNav} />
+      <About />
+      <Education />
+      <Experience />
+      <Achievements />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Chatbot />
     </div>
-  )
+  );
 };
 
 export default Main;
